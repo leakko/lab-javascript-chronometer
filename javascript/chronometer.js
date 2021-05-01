ca9cb27e3399250c93fdddd391f784ad90a94535
Marcos Salvo <marcossalvo95@gmail.com>
2021-05-01 22:55:44 +0200

    Just starting with iteration 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,9 +1,24 @@
+let currentTime = 0;
+let currentMinute = 0;
+
 class Chronometer {
-  constructor() {
+  constructor(){
+    this.currentTime = 0;
+    this.intervalId = undefined;
+
     // ... your code goes here
   }
+
   startClick(callback) {
     // ... your code goes here
+    
+    const intervalId = setInterval(() => {
+      currentTime += 1;
+    }, 1000);
+
+    if (currentTime === 60) {
+      clearInterval(intervalId);
+    }
   }
   getMinutes() {
     // ... your code goes here
